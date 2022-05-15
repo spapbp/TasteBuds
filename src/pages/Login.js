cbb65d1f79edc2a5378b2a054e2b0ce8c8f8e6ea
Sage <spapbp045@gmail.com>
2022-05-15 14:47:04 -0400

    added to heroku

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,12 @@
-function Login() {
-    return (
-        <h1>LOGIN</h1>
-    )
-}
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+
+const clientId =
+
+    function Login() {
+        return (
+            <h1>LOGIN</h1>
+        )
+    }
 
 export default Login;
